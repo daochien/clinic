9c2d773a8c44eecf0b8f4cb7b256030bf3c5914a
Dao Van Chien - Muare <chiendaovan@muare.vn>
2020-08-14 14:42:02 +0700

    no message

--- resources/views/layouts/sidebar-menu.blade.php
+++ resources/views/layouts/sidebar-menu.blade.php
@@ -30,17 +30,17 @@
                     <span>{{ __('sidebar.admin_manage') }}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-small">
-                   
+
                     <router-link to="/admin/managers" class="dropdown-item">
-                        
+
                         <span>{{ __('sidebar.admin_list_manage') }}</span>
                     </router-link>
-                                
+
                     <router-link to="/admin/roles" class="dropdown-item">
-                        
+
                         <span>{{ __('sidebar.role') }}</span>
                     </router-link>
-                                                                        
+
                 </div>
             </li>
             <li class="nav-item dropdown">
