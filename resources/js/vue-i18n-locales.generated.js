c0a1c8792a0fb08d27600b789445cc22e9049b6a
Dung <nguyenanhdung83@gmail.com>
2020-08-20 05:37:06 +0700

    update en lang

--- resources/js/vue-i18n-locales.generated.js
+++ resources/js/vue-i18n-locales.generated.js
@@ -2,7 +2,70 @@ export default {
     "en": {
         "app": {
             "name": "Clinic portal",
-            "setting": "Setting"
+            "setting": "Setting",
+            "btn": {
+                "create": "Create",
+                "edit": "Edit",
+                "update": "Update",
+                "delete": "Delete",
+                "submit": "Submit"
+            },
+            "label": {
+                "description": "Description",
+                "operator": "Operator"
+            },
+            "form": {
+                "keyword": "Keyword",
+                "keyword_placeholder": "keyword",
+                "clear_form": "clear form",
+                "submit_form": "Search"
+            },
+            "clinic": {
+                "header": {
+                    "create": "Create clinic",
+                    "info": "Information",
+                    "list-user": "List Users"
+                },
+                "place_holder": {
+                    "name": "Name",
+                    "post_code": "Post code",
+                    "address": "Address",
+                    "description": "Description"
+                },
+                "name": "Name",
+                "post_code": "Post code",
+                "address": "Address",
+                "description": "Description",
+                "manage_user": "Manage user",
+                "users_count": "User count"
+            },
+            "user": {
+                "title": {
+                    "index": "List",
+                    "create": "Create",
+                    "edit": "Edit"
+                },
+                "place_holder": {
+                    "name": "Name",
+                    "email": "Email",
+                    "group": "Select Group",
+                    "clinic": "Select Clinic",
+                    "description": "Description",
+                    "level": "Select Level",
+                    "type": "Select type"
+                },
+                "remove_staff": "Remove staff",
+                "name": "Name",
+                "email": "Email",
+                "group": "Group",
+                "level": "Level",
+                "type": "Type",
+                "register_date": "Register date",
+                "last_login_date": "Last login",
+                "clinic_name": "Clinic",
+                "group_select_all": "All Group",
+                "clinic_select_all": "All Clinic"
+            }
         },
         "auth": {
             "failed": "These credentials do not match our records.",
