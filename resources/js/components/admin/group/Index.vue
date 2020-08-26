85e647c2b813cbbbc4cb2f13aa9d1ea520c58d17
chung1993 <chungcnt52dh1@gmail.com>
2020-08-26 23:50:32 +0700

    fix some bug in group

--- resources/js/components/admin/group/Index.vue
+++ resources/js/components/admin/group/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="content">
-    <div class="container-fluid">
+    <div class="container-fluid" v-if="this.$gate.isRoot()">
 
         <div class="page-header row no-gutters py-4">
             <div class="col-6 text-center text-sm-left mb-0">
                 <h3 class="page-title">{{ $t('group.group_list')}}</h3>
             </div>
-            <div class="col-6 text-center text-sm-right mb-0" v-if="this.$gate.isRoot()">
+            <div class="col-6 text-center text-sm-right mb-0" >
                 <div class="card-tools">
                         <router-link :to="{name:'add_group'}" class="btn btn-sm btn-primary">{{ $t('group.add_new')}}</router-link>
                 </div>
@@ -66,6 +66,10 @@
         </div>
 
     </div>
+
+      <div v-else>
+          <not-found></not-found>
+      </div>
   </section>
 </template>
 
