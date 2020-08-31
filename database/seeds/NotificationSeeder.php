8980599c06f3e0e1172379824419e60deb6a7e6d
nguyen.van.dat <nguyen.van.dat@alliedtechbase.com>
2020-08-31 16:56:42 +0900

    seeder

---
+++ database/seeds/NotificationSeeder.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class NotificationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            DB::beginTransaction();
+            $bodUser = \App\Models\User::where('email', 'bod@gmail.com')->first();
+            if (!$bodUser) {
+                $bodUser = \App\Models\User::insert([
+                    'name' => 'bod',
+                    'email' => 'bod@gmail.com',
+                    'password' => \Illuminate\Support\Facades\Hash::make(123123),
+                    'note' => 'bod',
+                    'description' => 'bod',
+                    'posittion' => \App\Models\User::POSITTION['BOD'],
+                ]);
+            }
+
+            $notificationsData = [];
+            for ($i = 1; $i <= 20; $i ++) {
+                $notificationsData[] = [
+                    'title' => "Notification $i",
+                    'content' => "Notification content ". now() . ' - id ' . $i,
+                    'confirm' => rand(0,1),
+                    'draft' => 0,
+                    'schedule_date' => now(),
+                    'created_by' => $bodUser->id,
+                ];
+            }
+            \App\Models\Notification::insert($notificationsData);
+            // create user webapp.
+            $webappUser = \App\Models\User::where('email', 'webapp@gmail.com')->first();
+            if (!$webappUser) {
+                $webappUser = DB::table('users')->insert([
+                    'id' => 9999,
+                    'name' => 'webapp',
+                    'email' => 'webapp@gmail.com',
+                    'password' => \Illuminate\Support\Facades\Hash::make(123123),
+                    'note' => 'webapp',
+                    'description' => 'webapp',
+                    'posittion' => 0,
+                ]);
+            }
+
+            $notificationUsersData = [];
+            $notifications = \App\Models\Notification::all();
+            foreach ($notifications as $notification) {
+                $notificationUsersData[] = [
+                    'user_id' => $webappUser->id,
+                    'notification_id' => $notification->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+
+            $notificationUsers = DB::table('notification_users')->insert($notificationUsersData);
+            DB::commit();
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+            DB::rollBack();
+        }
+    }
+}
