626419d902f86ca650934d9abd9f9269a55c194b
Dung <nguyenanhdung83@gmail.com>
2020-10-06 21:40:27 +0700

    fix feedback

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -41,7 +41,7 @@ class ResetPasswordController extends Controller
         return [
             'token' => 'required',
             'email' => 'required|email',
-            'password' => 'required|confirmed|min:6',
+            'password' => 'required|confirmed|min:6|max:50',
         ];
     }
 
