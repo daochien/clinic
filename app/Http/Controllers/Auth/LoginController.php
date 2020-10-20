149af466d8c479ac50a6f671b89a4b3d043a64e4
Tony Hoang <hoangnam.hust90@gmail.com>
2020-10-20 16:54:02 +0700

    2010201653 - tiny fix lang

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -72,7 +72,7 @@ class LoginController extends Controller
 
         if ($authUser->isMobileUser()) {
             Auth::guard('web')->logout();
-            Toastr::error(__('auth.login.other._staff_mobile_user_cant_login'));
+            Toastr::error(__('auth.login.others._staff_mobile_user_cant_login'));
             return redirect('/login');
         } elseif ($authUser->isWebUser()) {
             LoginLog::create(['user_id' => $authUser->id]);
